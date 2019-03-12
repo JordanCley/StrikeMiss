77de768db802dab57030f838b76312968272bfed
Jordan McQuiston <jordan@jordancley.com>
2019-03-12 04:29:34 -0700

    took away input on edit route and added required attr to NEW.ejs

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.static("public"));
 app.set("view engine" , "ejs");
 app.use(methodOverride("_method"));
 
+
 // SCHEMA SETUP
 var bowlerSchema = new mongoose.Schema({
     bowler: "String",
